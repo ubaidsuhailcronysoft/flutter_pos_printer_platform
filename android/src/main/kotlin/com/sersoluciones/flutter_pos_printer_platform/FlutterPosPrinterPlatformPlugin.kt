adc0d45e58f1d921dcb0d312c868f19234d53777
ubaidsuhailcronysoft <157352477+ubaidsuhailcronysoft@users.noreply.github.com>
2026-09-16 12:19:33 +0500

    bluetooth conection toast commented

--- android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/FlutterPosPrinterPlatformPlugin.kt
+++ android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/FlutterPosPrinterPlatformPlugin.kt
@@ -142,7 +142,7 @@ class FlutterPosPrinterPlatformPlugin : FlutterPlugin, MethodCallHandler, Plugin
                 BluetoothConstants.MESSAGE_TOAST -> {
                     val bundle = msg.data
                     bundle?.getInt(BluetoothConnection.TOAST)?.let {
-                        Toast.makeText(context, context!!.getString(it), Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(context, context!!.getString(it), Toast.LENGTH_SHORT).show()
                     }
                 }
                 BluetoothConstants.MESSAGE_START_SCANNING -> {
